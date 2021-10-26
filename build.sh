415c67e5fe561f199de983d33a8f09a7c96459e2
Adam Wilmore <adam@mx51.io>
2021-10-26 11:05:52 +1100

    Updated build and push scripts

--- build.sh
+++ build.sh
@@ -2,4 +2,49 @@
 
 set -e
 
-docker buildx build -t awilmore/k9s:latest . --load
+###
+# FUNCTIONS
+###
+
+check_tag_value() {
+  # Use git branch name for tag
+  if [ -z "$TAG_NAME" ]; then
+    # Check branch name for tag value
+    BRANCH_NAME=$( git rev-parse --symbolic-full-name --abbrev-ref HEAD 2>/dev/null )
+
+    if [ -z "$BRANCH_NAME" ] || [ "$BRANCH_NAME" == "HEAD" ]; then
+      echo "error: git project not detected, or not initalised properly"
+      die "expecting valid tag/branch name (value was either HEAD or not present)."
+    fi
+
+    export TAG_NAME="$BRANCH_NAME"
+  fi
+}
+
+die() {
+  msg="$1"
+  echo -e "$msg\nAborting."
+  exit 1
+}
+
+
+###
+# MAIN
+###
+
+# Determine tag name
+check_tag_value
+
+# Build image
+echo
+echo " * Building k9s image version: $TAG_NAME ..."
+echo
+
+docker buildx build \
+  -t awilmore/k9s:latest \
+  -t awilmore/k9s:$TAG_NAME \
+  . --load
+
+echo
+echo " * Done."
+echo
--- push.sh
+++ push.sh
@@ -2,4 +2,47 @@
 
 set -e
 
+###
+# FUNCTIONS
+###
+
+check_tag_value() {
+  # Use git branch name for tag
+  if [ -z "$TAG_NAME" ]; then
+    # Check branch name for tag value
+    BRANCH_NAME=$( git rev-parse --symbolic-full-name --abbrev-ref HEAD 2>/dev/null )
+
+    if [ -z "$BRANCH_NAME" ] || [ "$BRANCH_NAME" == "HEAD" ]; then
+      echo "error: git project not detected, or not initalised properly"
+      die "expecting valid tag/branch name (value was either HEAD or not present)."
+    fi
+
+    export TAG_NAME="$BRANCH_NAME"
+  fi
+}
+
+die() {
+  msg="$1"
+  echo -e "$msg\nAborting."
+  exit 1
+}
+
+
+###
+# MAIN
+###
+
+# Determine tag name
+check_tag_value
+
+# Build image
+echo
+echo " * Pushing k9s image version: $TAG_NAME ..."
+echo
+
+docker push awilmore/k9s:$TAG_NAME
 docker push awilmore/k9s:latest
+
+echo
+echo " * Done."
+echo
